b0e51106ce8a82fa235b610ee23a3d4b627adc52
yididiya <yididiya07@gmail.com>
2023-11-04 09:59:52 +0300

    adding dashboard

---
+++ lib/dashboard.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:weldwahidapp/deber.dart';
+import 'NavBar.dart';
+
+class Dashboard extends StatelessWidget {
+   Dashboard({super.key});
+     var height, width;
+
+     List Imgdata=[
+     "assets/bete.png",
+      "assets/logo.png",
+      "assets/keb.png",
+      "assets/StYared.jpg",
+      "assets/keb.png",
+      "assets/location.png",
+    
+     ];
+     List titles=[
+      "ስለ ደብሩ",
+      "ሰ/ት/ቤቱ ",
+      "መዝሙራት",
+      "ወረቦች",
+      "yeneseha mezmur",
+      "አድራሻዎቻችን",
+     
+
+     ];
+     List pages=[
+    
+      Deber(),
+      NavBar(),
+      NavBar(),
+      NavBar(),
+      NavBar(),
+      NavBar(),
+      
+      
+     ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    height= MediaQuery.of(context).size.height;
+    width= MediaQuery.of(context).size.width;
+    return Scaffold(
+      drawer: NavBar(),
+      body: Container(
+        color: HexColor("#1d4518"),
+        height: height,
+        width: width,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+               color: HexColor("#1d4518"),
+              ),
+              height: height*0.25,
+              width: width,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 30,
+                      left:15,
+                      right: 15,
+
+                    ),
+                    child: Row
+                    (mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      InkWell(
+                        onTap: () {
+                       Navigator.of(context).push(MaterialPageRoute(builder: (context) => NavBar()));
+
+                        },
+                        child:const Icon(
+                          Icons.sort,
+                          color: Colors.white,
+                          size:40,
+                        ),
+                      ),
+                      Container(
+                         height: 50,width: 50,
+                        decoration:  const BoxDecoration(
+                          image:DecorationImage(image: AssetImage( "assets/logo.png"))
+                        ),
+
+                       
+                      )
+                    ],),
+                  ),
+                  
+                    Padding(
+                    padding:const EdgeInsets.only(
+                      top: 20,
+                      left:15,
+                      right: 15,
+                    ),
+                    child:  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children:const [
+                        Text(
+                          "ወልድ ዋህድ መተግበርያ",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            
+                           
+                          ),
+                        ),
+                        Text(
+                          "በስራ ላይ....",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white38,
+                            
+                            
+                           
+                          ),
+                        )
+                      ],
+
+                    )
+                    ,),
+
+
+                ]),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30)
+                )
+              ),
+              height: height*0.75,
+              width: width,
+              child:
+               GridView.builder(
+               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                   crossAxisCount:2,
+                   childAspectRatio: 1.1,
+                   mainAxisSpacing: 5 
+                   ),
+              
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: Imgdata.length,
+              itemBuilder: (context,index){
+               return Padding(
+                 padding: const EdgeInsets.only(
+                  top: 15
+                 ),
+                 child: InkWell(
+                   onTap: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context)=>pages[index]));
+  
+                   },
+                   child: Container(
+                     margin:const EdgeInsets.symmetric(vertical:8, horizontal:20,
+
+                   ),
+                   decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(15),
+                     color: Colors.white,
+                     boxShadow: const [
+                       BoxShadow(
+                         color: Color.fromARGB(66, 85, 85, 85),
+                         spreadRadius: 1,
+                         blurRadius: 6,
+                       )
+                     ]
+                   ),
+                   child: Column(
+                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                     children: [
+                       Image.asset(Imgdata[index],
+                       width: 80,
+                       height: 70,
+                       ),
+                       Text(titles[index],
+                        style:const TextStyle(
+                            fontSize: 10,
+                  
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                        )
+                       
+                       ),
+                     ],
+                   ),
+                   )
+                   
+
+                 ),
+               );
+              },
+              ),
+              
+            )
+          ],)
+      ),
+      
+    );
+  } 
+}
